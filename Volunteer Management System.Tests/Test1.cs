4e2c8e349a4bbe4c89468bb45cf48d105d8ab7bd
pwv8669 <pwv8669@autuni.ac.nz>
2026-08-12 13:01:56 +1200

    Add new test project and enable parallel test execution
    A new test project, `Volunteer Management System.Tests`, has been
    added to the solution. The project includes a basic test class
    (`Test1`) with an empty test method (`TestMethod1`) for initial
    setup.
    
    Parallel test execution has been enabled in `MSTestSettings.cs`
    using the `[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]`
    attribute to improve test performance.
    
    The test project targets `.NET 10.0`, uses the latest C# language
    version, and includes the `MSTest` package (v4.0.1) for unit
    testing. Nullable reference types and implicit `using` directives
    are also enabled.

---
+++ Volunteer Management System.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Volunteer_Management_System.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
